999c7d15a340917d21b5660606766d078dd963d5
Jess Jacobson <jessanova@gmail.com>
2018-06-05 15:09:12 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,21 +1,26 @@
 // declare your functions here...
 
 function paragraphSelector(){
+  console.log($('p'))
   return $('p')
 }
 
 lastImageSelector(){
+  console.log($('img:last'))
   return $('img:last')
 }
 
 ninjaBabySelector(){
+  console.log($('img#baby-ninja'))
   return $('img#baby-ninja')
 }
 
 divSelector(){
+  console.log($('.pics'))
   return $('.pics')
 }
 
 firstListItem(){
+  console.log($('ul #pic-list:first-child'))
   return $('ul #pic-list:first-child')
 }
